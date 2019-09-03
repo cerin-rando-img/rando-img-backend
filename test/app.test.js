467b740764bbab0ad9a1ff9b94789fb8851d4e52
Erin Gurley <erinmgurley@gmail.com>
2019-09-03 11:10:22 -0700

    get by id test passing!

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Pokemon = require('../lib/models/Pokemon');
+
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +18,18 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+  
+  it('get a pokemon by id', async() => {
+    const pokemon = await Pokemon.create({ pokemon: 'venusaur' });
+    return request(app)
+      .get(`/api/v1/pokemon/${pokemon._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          pokemon: 'venusaur',
+          __v: 0
+        });
+      });   
   });
 });
